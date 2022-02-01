9031ab3a98c06b50c8398ae77707dc4ec81f9e22
Gustavo Apolonio <gustta.apolonio@gmail.com>
2022-02-01 17:37:39 -0300

    Project's Front -- Static Charts

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,5 +1,7 @@
 import 'assets/css/styles.css';
+import BarChart from 'components/barChart';
 import DataTable from 'components/dataTable';
+import DonutChart from 'components/donutChart';
 import Footer from 'components/footer';
 import Navbar from 'components/navbar';
 
@@ -8,9 +10,31 @@ function App() {
     <>
       <Navbar />
       <div className='container'>
-        <h1 className="text-primary">
-          Olá mundo!
+        <h1 className="text-primary py-3">
+          Dashboard de Vendas
         </h1>
+
+        <div className="row px-3">
+          <div className="col-sm-6">
+            <h5 className='text-center text-secondary'>
+              Taxa de Sucesso (%)
+            </h5>
+            <BarChart />
+          </div>
+          <div className="col-sm-6">
+            <h5 className='text-center text-secondary'>
+              Todas vendas
+            </h5>
+            <DonutChart />
+          </div>
+        </div>
+
+        <div className="py-3">
+          <h2 className='text-primary'>
+            Todas vendas
+          </h2>
+        </div>
+
         <DataTable />
       </div>
       <Footer />
